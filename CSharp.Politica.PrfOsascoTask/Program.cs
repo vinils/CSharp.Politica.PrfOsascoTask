df0bb1a12c30a6e17ce8dbe2ffd4f81b33bfd7c0
vlsantos <vinicius.lourenco@gmail.com>
2019-02-28 23:37:45 -0300

    improving code

--- CSharp.Politica.PrfOsascoTask/Program.cs
+++ CSharp.Politica.PrfOsascoTask/Program.cs
@@ -1,11 +1,36 @@
 namespace CSharp.Politica.PrfOsascoTask
 {
+    using Data;
     using RestSharp;
     using System;
-    using System.Linq;
     using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
     using System.Text;
-    using Data;
+
+    public class ViagensXml
+    {
+        public int Nro_Processo { get; set; }
+        public string Destino { get; set; }
+        public DateTime Inicio { get; set; }
+        public DateTime Fim { get; set; }
+        public decimal Valor { get; set; }
+        public string Motivo { get; set; }
+        public string Observacao { get; set; }
+        public string Orgao { get; set; }
+        public string Unidade { get; set; }
+    }
+
+    public class ViagensCsv
+    {
+        public int Nro_Processo { get; set; }
+        public string Destino { get; set; }
+        public decimal Valor { get; set; }
+        public string Motivo { get; set; }
+        public string Observacao { get; set; }
+        public string Nome { get; set; }
+        public string Cargo { get; set; }
+    }
 
     public class GroupNameTree
     {
@@ -37,18 +62,10 @@ namespace CSharp.Politica.PrfOsascoTask
         public decimal Value { get; set; }
     }
 
-    public class body
-    {
-        public GroupNameTree Groups { get; set; }
-    }
-
-    public class body2
-    {
-        public List<Data.Models.Exam> Exams { get; set; }
-    }
-
     class Program
     {
+        private static readonly string[] rootPath = new string[] { "Política", "Prefeituras", "Osasco" };
+        private static readonly Guid OsascoGroupId = new Guid("0AA16E58-12C8-4BB5-9F18-F8BDB9914259");
         public static bool IsCnpj(string cnpj)
         {
             int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
@@ -145,21 +162,251 @@ namespace CSharp.Politica.PrfOsascoTask
                 .ToUpper();
         }
 
-        static void Main(string[] args)
+        public static void BulkInsertViagens()
         {
             Func<string, string> treatIndexLocal = treatIndex;
-            var year = 2019;
-            var month = 1;
 
-            GetData(year, month, out List<ExamPrfOsasco> exams, out DictionaryTree<string, string> groups, treatIndexLocal);
-            GroupBulkInsertByName(groups);
-            ExamBulkInsert(exams, treatIndex);
+            for (var year = 2018; year >= 0; year--)
+            {
+                var count = 100;
+                while (count >= 0)
+                {
+                    count--;
+                    try
+                    {
+                        GetViagens(
+                            year,
+                            out List<ExamPrfOsasco> exams,
+                            out DictionaryTree<string, string> groups,
+                            treatIndexLocal);
+                        //GroupBulkInsertByName(groups);
+                        //ExamBulkInsert(exams, treatIndex);
+                        break;
+                    }
+                    catch (Exception)
+                    {
+                        System.Threading.Thread.Sleep(30 * 60 * 1000);
+                    }
+                }
+            }
         }
 
-        private static void GetData(int year, int month, out List<ExamPrfOsasco> exams, out DictionaryTree<string, string> groups, Func<string, string> treatIndex)
+        public static void BulkInsertDespesas()
+        {
+            Func<string, string> treatIndexLocal = treatIndex;
+
+            for (var year = 2018; year >= 0; year--)
+            {
+                for (var month = 12; month >= 1; month--)
+                {
+                    var count = 100;
+                    while (count >= 0)
+                    {
+                        count--;
+                        try
+                        {
+                            GetDespesas(
+                                year, 
+                                month, 
+                                out List<ExamPrfOsasco> exams, 
+                                out DictionaryTree<string, string> groups, 
+                                treatIndexLocal);
+                            GroupBulkInsertByName(groups);
+                            ExamBulkInsert(exams, treatIndex);
+                            break;
+                        }
+                        catch (Exception)
+                        {
+                            System.Threading.Thread.Sleep(30 * 60 * 1000);
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            BulkInsertDespesas();
+            BulkInsertViagens();
+        }
+
+        private static List<ViagensCsv> GetViagensCsv(int year)
+        {
+            var parameters = "{" + $"\"edtExercicio\":{year},\"edtProcesso\":\"%\",\"edtDestino\":\"%\"" + "}";
+            var parametersEncoded = Encoding.UTF8.GetBytes(parameters);
+            var parametersBase64 = Convert.ToBase64String(parametersEncoded);
+            var url = "http://eportal.osasco.sp.gov.br/eportais-api/api/relatorio/gerarRelatorioParam/RelacaoDespesaViagem/CSV/" + parametersBase64;
+            var request = new RestRequest(Method.GET);
+            var response = new RestClient(url)
+                .Execute(request);
+
+            if(response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new Exception(response.Content);
+            }
+
+            var viagens = new List<ViagensCsv>();
+
+            using (var stream = new System.IO.MemoryStream(response.RawBytes))
+            using (var rd = new System.IO.StreamReader(stream, Encoding.UTF7))
+            {
+                string row = null;
+
+                while ((row = rd.ReadLine()) != null)
+                {
+                    if(row.StartsWith("Ano"))
+                        continue;
+
+                    var columns = row.Split(';');
+
+                    try
+                    {
+                        //var tmpPeriodo = columns[3].Split(' ');
+                        //var dtInicio = DateTime.Parse(tmpPeriodo[0]);
+                        //var dtFim = DateTime.Parse(tmpPeriodo.Last());
+                        var valor = decimal.Parse(columns[5], CultureInfo.InvariantCulture);
+                        var viagem = new ViagensCsv()
+                        {
+                            Nro_Processo = int.Parse(columns[1]),
+                            Destino = columns[2],
+                            Valor = valor,
+                            Motivo = columns[6],
+                            Observacao = columns[7],
+                            Nome = columns[8],
+                            Cargo = columns[9]
+                        };
+
+                        viagens.Add(viagem);
+                    }
+                    catch (Exception ex)
+                    {
+
+                        throw ex;
+                    }
+                }
+            }
+
+            return viagens;
+        }
+
+        private static List<ViagensXml> GetViagensXml(int year)
+        {
+            var parameters = "{" + $"\"edtExercicio\":{year},\"edtProcesso\":\"%\",\"edtDestino\":\"%\"" + "}";
+            var parametersEncoded = Encoding.UTF8.GetBytes(parameters);
+            var parametersBase64 = Convert.ToBase64String(parametersEncoded);
+            var client = new RestClient("http://eportal.osasco.sp.gov.br/eportais-api/api/relatorio/gerarRelatorioXML/Relacao%20Despesa%20Viagem%20Osasco/" + parametersBase64);
+            var request = new RestRequest(Method.GET);
+            var response = client.Execute(request);
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                throw new Exception(response.Content);
+
+            var viagens = new List<ViagensXml>();
+            var xml = response.Content;
+            var xmlDoc = new System.Xml.XmlDocument();
+            xmlDoc.LoadXml(xml);
+
+            var xpath = "RelacaoDespesaViagemOsasco/registro";
+            var nodes = xmlDoc.SelectNodes(xpath);
+
+            try
+            {
+                foreach (System.Xml.XmlNode childrenNode in nodes)
+                {
+                    var valor = childrenNode.SelectSingleNode("vl_medio_participante").InnerText;
+
+                    if (valor.Length >= 30)
+                        valor = valor.Substring(0, 30);
+
+                    var viagem = new ViagensXml()
+                    {
+                        Nro_Processo = int.Parse(childrenNode.SelectSingleNode("nr_processo").InnerText),
+                        Destino = childrenNode.SelectSingleNode("ds_destinoviagem").InnerText,
+                        Motivo = childrenNode.SelectSingleNode("ds_motivo").InnerText,
+                        Observacao = childrenNode.SelectSingleNode("ds_observacao").InnerText,
+                        Orgao = childrenNode.SelectSingleNode("ds_orgao").InnerText,
+                        Unidade = childrenNode.SelectSingleNode("ds_unidade").InnerText,
+                        Valor = decimal.Parse(valor, CultureInfo.InvariantCulture),
+                        Inicio = DateTime.Parse(childrenNode.SelectSingleNode("dt_inicio").InnerText),
+                        Fim = DateTime.Parse(childrenNode.SelectSingleNode("dt_termino").InnerText)
+                    };
+
+                    viagens.Add(viagem);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            return viagens;
+        }
+
+        private static void GetViagens(int year, out List<ExamPrfOsasco> exams, out DictionaryTree<string, string> despesas, Func<string, string> treatIndex)
         {
             exams = new List<ExamPrfOsasco>();
-            groups = new DictionaryTree<string, string>(s => treatIndex(s), "Política");
+            despesas = new DictionaryTree<string, string>(s => treatIndex(s), "Viagens");
+
+            var viagensCsv = GetViagensCsv(year);
+            var viagensXml = GetViagensXml(year);
+
+            var viagens = from x in viagensXml
+                           join c in viagensCsv on new { x.Nro_Processo } equals new { c.Nro_Processo }
+                           select new { x.Destino, x.Motivo, x.Observacao, x.Valor, x.Inicio, x.Fim, x.Orgao, x.Unidade, c.Nome, c.Cargo };
+            
+            if (viagensCsv.Count() != viagens.Count())
+                throw new Exception("Missing data");
+
+            foreach (var viagem in viagens)
+            {
+                var groupValor = despesas.AddIfNew(
+                    viagem.Orgao,
+                    viagem.Unidade,
+                    viagem.Destino,
+                    viagem.Motivo,
+                    viagem.Observacao,
+                    viagem.Cargo,
+                    viagem.Nome,
+                    "Valor");
+
+                var examValor = new ExamPrfOsasco()
+                {
+                    Date = viagem.Inicio,
+                    Group = groupValor,
+                    Value = viagem.Valor
+                };
+
+                exams.Add(examValor);
+
+                var groupDias = despesas.AddIfNew(
+                    "Prefeituras",
+                    "Osasco",
+                    "Viagens",
+                    viagem.Orgao,
+                    viagem.Unidade,
+                    viagem.Destino,
+                    viagem.Motivo,
+                    viagem.Observacao,
+                    viagem.Cargo,
+                    viagem.Nome,
+                    "Dias");
+
+                var examDias = new ExamPrfOsasco()
+                {
+                    Date = viagem.Inicio,
+                    Group = groupDias,
+                    Value = viagem.Valor
+                };
+
+                exams.Add(examDias);
+            }
+        }
+
+        private static void GetDespesas(int year, int month, out List<ExamPrfOsasco> exams, out DictionaryTree<string, string> despesas, Func<string, string> treatIndex)
+        {
+            exams = new List<ExamPrfOsasco>();
+            despesas = new DictionaryTree<string, string>(s => treatIndex(s), "Despesas");
 
             var parameters = "{" + $"\"edtExercicio\":{year},\"edtMes\":{month},\"edtCategoria\":\"%\"" + "}";
             var parametersEncoded = Encoding.UTF8.GetBytes(parameters);
@@ -183,7 +430,7 @@ namespace CSharp.Politica.PrfOsascoTask
                 var year2 = int.Parse(childrenNode.SelectSingleNode("nr_ano").InnerText);
                 var month2 = int.Parse(childrenNode.SelectSingleNode("nr_mes").InnerText);
                 var date = new DateTime(year2, month2, 1);
-                var valor = decimal.Parse(childrenNode.SelectSingleNode("vl_pagamento").InnerText);
+                var valor = decimal.Parse(childrenNode.SelectSingleNode("vl_pagamento").InnerText, CultureInfo.InvariantCulture);
                 var cpfCnpj = childrenNode.SelectSingleNode("cnpj").InnerText;
                 var tpPessoa = "Indefinido";
 
@@ -196,10 +443,7 @@ namespace CSharp.Politica.PrfOsascoTask
                     tpPessoa = "Empresa";
                 }
 
-                var group = groups.AddIfNew(
-                    "Prefeituras",
-                    "Osasco",
-                    "Despesas",
+                var despesa = despesas.AddIfNew(
                     childrenNode.SelectSingleNode("ds_categoria").InnerText,
                     childrenNode.SelectSingleNode("ds_grupo").InnerText,
                     childrenNode.SelectSingleNode("ds_modalidade").InnerText,
@@ -212,7 +456,7 @@ namespace CSharp.Politica.PrfOsascoTask
                 var exam = new ExamPrfOsasco()
                 {
                     Date = date,
-                    Group = group,
+                    Group = despesa,
                     Value = valor
                 };
 
@@ -244,17 +488,22 @@ namespace CSharp.Politica.PrfOsascoTask
             if (!groups.Any())
                 return;
 
-            var body = new body() { Groups = (GroupNameTree)groups };
-            var json = Newtonsoft.Json.JsonConvert.SerializeObject(body);
-            var client2 = new RestClient("http://localhost:58994/odata/v4/groups/BulkInsertByName");
-            var request2 = new RestRequest(Method.POST);
-            request2.RequestFormat = DataFormat.Json;
-            request2.AddJsonBody(json);
-            var response2 = client2.Execute(request2);
-
-            if (response2.StatusCode != System.Net.HttpStatusCode.OK)
+            var request = new RestRequest(Method.POST)
             {
-                throw new Exception(response2.Content);
+                Timeout = int.MaxValue,
+                RequestFormat = DataFormat.Json
+            };
+            var body = new { Groups = (GroupNameTree)groups , Root = rootPath };
+
+            request.AddJsonBody(body);
+
+            var url = "http://localhost:58994/odata/v4/groups/BulkInsertByName";
+            var response = new RestClient(url)
+                .Execute(request);
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new Exception(response.Content);
             }
         }
 
@@ -265,10 +514,12 @@ namespace CSharp.Politica.PrfOsascoTask
 
             var dataUriStr = "http://localhost:58994/odata/v4";
             var dataUri = new Uri(dataUriStr);
-            var container = new Default.Container(dataUri);
-            container.Timeout = int.MaxValue;
+            var container = new Default.Container(dataUri)
+            {
+                Timeout = int.MaxValue
+            };
 
-            var groupsDbDictionary = container.Groups.ToDictionaryTree(g => treatIndex(g.Name));
+            var groupsDbDictionary = container.Groups.ToDictionaryTree(g => treatIndex(g.Name), OsascoGroupId);
             var exams2 = exams
                 .GroupBy(e => string.Join("/", e.Group.Key) + "/" + e.Date.ToString())
                 .Select(eg =>
